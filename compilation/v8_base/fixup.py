4981894acb3e9fa5a8773185ea469377237918a3
Peter Schmidt-Nielsen <peter.schmidt-nielsen@cba.mit.edu>
2012-10-19 14:44:33 -0400

    Cleaned up fixup.py

--- compilation/v8_base/fixup.py
+++ compilation/v8_base/fixup.py
@@ -1,8 +1,20 @@
 #! /usr/bin/python
+# To build `prod' as used by this.
+# These commands should also be in the makefile under the recipe for prod -- use those instead if these disagree.
 # objcopy --input binary --output elf64-x86-64 --binary-architecture i386:x86-64 --rename-section .data=.unicorns code.js javascript.o
 # g++ -o prod launch.o opengl.o os.o javascript.o libs64/libv8_base.a libs64/libv8_snapshot.a -lpthread -lGLU -lSDL
 
 import struct, pprint
+
+# Magic colored text constants
+normal = "\x1B\x5B\x30\x6D"
+grey   = "\x1B\x5B\x30\x31\x3B\x33\x30\x6D"
+red    = "\x1B\x5B\x30\x31\x3B\x33\x31\x6D"
+green  = "\x1B\x5B\x30\x31\x3B\x33\x32\x6D"
+yellow = "\x1B\x5B\x30\x31\x3B\x33\x33\x6D"
+blue   = "\x1B\x5B\x30\x31\x3B\x33\x34\x6D"
+purple = "\x1B\x5B\x30\x31\x3B\x33\x35\x6D"
+teal   = "\x1B\x5B\x30\x31\x3B\x33\x36\x6D"
 
 half, word = "H", "I"
 xword = addr = off = "Q"
@@ -79,9 +91,13 @@ def read_str(data, index):
 	while data[i] != "\0": i += 1
 	return data[index:i]
 
+# Read in our ELF binary.
 data = open("prod").read()
 header = parse(elf_header_format, data)
+print green + "ELF header:" + normal
 pprint.pprint(header)
+
+# Read in the section header table.
 addr = header["e_shoff"]
 section_headers = []
 for i in xrange(header["e_shnum"]):
@@ -90,6 +106,7 @@ for i in xrange(header["e_shnum"]):
 	section_headers.append(section)
 	addr += header["e_shentsize"]
 
+# Read in the program header table.
 addr = header["e_phoff"]
 program_headers = []
 for i in xrange(header["e_phnum"]):
@@ -98,98 +115,88 @@ for i in xrange(header["e_phnum"]):
 	program_headers.append(program)
 	addr += header["e_phentsize"]
 
+# Figure out where section names are.
 section_strings_base = data[section_headers[header["e_shstrndx"]]["sh_offset"]:]
 
+# Figure out where symbol names are.
 for section in section_headers:
 	section["sh_name"] = read_str(section_strings_base, section["sh_name"])
 	if section["sh_name"] == ".strtab":
 		strings_base = data[section["sh_offset"]:]
 
+# Pad the binary out to a multiple of 32 bytes.
 padding = -len(data)%32
-print "Padding with", padding, "null bytes."
+print green + "Padding binary out to multiple of 32 with null bytes:" + normal, padding
 data += "\0" * padding
-end_of_binary_addr = struct.pack("<Q", len(data))
 
-locs = []
+# These are places where the length of the Javascript program has to be added.
+relocation_addresses = []
 
-reasonable_vma = 0
-
-#NOTUSED
-# Find a reasonable VMA to dump to.
-for header in program_headers:
-	max_hit = header["p_vaddr"] + header["p_memsz"]
-	reasonable_vma = max(max_hit, reasonable_vma)
-
+# Find the difference between 
 for section in section_headers:
 	if section["sh_name"] == ".unicorns":
-		print "Found .unicorns section."
+		print green + "Found .unicorns section." + normal
 		offset, size = section["sh_offset"], section["sh_size"]
 		addr = section["OFFSET"]
 		# Compute the ELF load offset.
 		elf_load_offset = section["sh_addr"] - offset
-		print "ELF load offset:", hex(elf_load_offset)
+		print green + "ELF load offset:" + normal, hex(elf_load_offset)
 		# Don't edit this section of the ELF!
 		# It appears to only be link-time information, and doesn't do what we need.
-#		patches.append( (addr+24, addr+24+8, end_of_binary_addr) )
-#		patches.append( (addr+32, addr+32+8, "\0"*8) )
-#		patch_address = addr+32
 		pprint.pprint(section)
-#		data[offset]
-		#print repr(data[offset:offset+size])
-	# Find the section with type SHT_SYMTAB.
 
-# Round the VMA up to have the appropriate offset.
-reasonable_vma = len(data) + elf_load_offset
+# The address at which the Javascript program will be loaded.
+javascript_load_vma = len(data) + elf_load_offset
 
-print "Reasonable VMA:", hex(reasonable_vma)
+print green + "Javascript load VMA:" + normal, hex(javascript_load_vma)
 
+SHT_SYMTAB = 2
+
+# Find the symbol table (SYMTAB) section to modify the .unicorns pointer, _binary_code_js_start.
 for section in section_headers:
-	if section["sh_type"] == 2:
+	if section["sh_type"] == SHT_SYMTAB:
 		addr = section["sh_offset"]
 		fs = format_size(symbol_entry_format)
+		# Read through the symbol tale.
 		while addr < section["sh_offset"] + section["sh_size"]:
 			entry = parse(symbol_entry_format, data[addr:])
 			entry["st_name"] = read_str(strings_base, entry["st_name"])
-			if entry["st_name"] in ("_binary_code_js_start", "_binary_code_js_end"):
-#				patches.append( (addr+8, addr+8+8, end_of_binary_addr) )
-				if entry["st_name"] == "_binary_code_js_start":
-					pointer_target = entry["st_value"]
-					file_offset = entry["st_value"] - elf_load_offset
-					patches.append( (file_offset, file_offset+8, struct.pack("<Q", reasonable_vma)) )
-					#patches.append( (file_offset+8, file_offset+16, "\0"*8) )
-					locs.append(file_offset+8)
-#				if entry["st_name"] == "_binary_code_js_end":
-#					add_length = addr+8
-				print "Found symbol to patch."
+			if entry["st_name"] == "_binary_code_js_start":
+				print green + "Found symbol to edit:" + normal
 				pprint.pprint(entry)
+				pointer_target = entry["st_value"]
+				file_offset = entry["st_value"] - elf_load_offset
+				patches.append( (file_offset, file_offset+8, struct.pack("<Q", javascript_load_vma)) )
+				patches.append( (file_offset+8, file_offset+16, "\0"*8) )
+				relocation_addresses.append(file_offset+8)
+				break
 			addr += fs
 
 PT_LOAD  = 1
 PT_RELRO = 0x6474e552
 
+# Hack the RELRO program header table entry into a PT_LOAD entry that loads our desired
 for header in program_headers:
 	if header["p_type"] == PT_RELRO:
-		print "Found program header to mangle:"
+		print green + "Found program header to mangle:" + normal
 		pprint.pprint(header)
 		header["p_align"] = 2**3
-		header["p_flags"] = 6
+		header["p_flags"] = 6 # rw-
 		header["p_type"] = PT_LOAD
 		header["p_memsz"] = 0
 		header["p_offset"] = len(data)
-		header["p_vaddr"] = header["p_paddr"] = reasonable_vma
+		header["p_vaddr"] = header["p_paddr"] = javascript_load_vma
 		header["p_filesz"] = 0
-#		shy = hex(len(data) - (header["p_filesz"] + header["p_offset"]))
-#		print "eof %s - (offset %s + size %s) = %s" % (hex(len(data)), hex(header["p_offset"]), hex(header["p_filesz"]), shy)
-#		patches.append( (header["OFFSET"]+32, header["OFFSET"]+40, struct.pack("<Q", len(data) - header["p_offset"])) )
-#		patches.append( (header["OFFSET"]+40, header["OFFSET"]+48, struct.pack("<Q", len(data) - header["p_offset"])) )
-		locs.append(header["OFFSET"]+32)
-		locs.append(header["OFFSET"]+40)
-		print "Newly mangled:"
+		relocation_addresses.append(header["OFFSET"]+32) # 
+		relocation_addresses.append(header["OFFSET"]+40)
+		print green + "Newly mangled:" + normal
 		pprint.pprint(header)
 		write_back(program_header_format, header)
 
 # Patch-up the binary.
-print "Patching up."
+
+print
+print green + "Binary patches:" + normal
 data = list(data)
 for start, stop, new in patches:
 	previous = "".join(data[start:stop])
@@ -203,9 +210,12 @@ fd = open("../quick_links/elf64_data", "w")
 fd.write(data)
 fd.close()
 
+print
+print green + "Binary relocations:" + normal
+
 fd = open("../quick_links/elf64_relocs", "w")
-for loc in locs:
-	print "Adding relocation at:", hex(loc)
+for loc in relocation_addresses:
+	print "\t[%08x:%08x] += js_size" % (loc, loc+8)
 	fd.write("%s,8,js_size\n" % loc)
 #fd.write("%s,8,js_size\n" % (add_length,))
 fd.close()
